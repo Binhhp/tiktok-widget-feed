7f46035620f37350dbf8c2e8292e27e7c44fe38a
khoapiterrr <khoapiterrr99@gmail.com>
2022-11-09 00:26:18 +0700

    call api post

--- TiktokWidget/ClientApp/src/repositories/dtos/responses/IPost.ts
+++ TiktokWidget/ClientApp/src/repositories/dtos/responses/IPost.ts
@@ -2,11 +2,14 @@ export interface IPost {
   image?: string;
   title: string;
   description: string;
+  content?: string;
   url: string;
   status?: boolean;
   ordering?: string;
   createdTime?: string;
   id: string;
+  impression: number;
+  clicks: number;
 }
 export interface IPostResponse {
   '@odata.context': string;
