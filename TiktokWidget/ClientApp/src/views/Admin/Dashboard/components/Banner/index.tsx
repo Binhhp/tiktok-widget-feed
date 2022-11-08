79738f108597a4cb1622d4ea1d270cdbf10e83ff
binhhp <Binhhp20@gmail.com>
2022-11-08 22:57:35 +0700

    update banner

--- TiktokWidget/ClientApp/src/views/Admin/Dashboard/components/Banner/index.tsx
+++ TiktokWidget/ClientApp/src/views/Admin/Dashboard/components/Banner/index.tsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootComponent } from './style';
-import { RootReducer } from 'stores/Admin/reducers';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootComponent } from "./style";
+import { RootReducer } from "stores/Admin/reducers";
 //Swiper
-import SwiperCore, { Autoplay, Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import useSWR from 'swr';
+import SwiperCore, { Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import useSWR from "swr";
 // Import Swiper styles
-import { getBanners } from 'repositories/api';
-SwiperCore.use([Navigation]);
+import { getBanners } from "repositories/api";
+SwiperCore.use([Autoplay, Pagination]);
 
 const Banner = () => {
   const shopReducer = useSelector((state: RootReducer) => state.ShopReducer);
-  const { data } = useSWR('/odata/Banner', getBanners);
-  console.log({ data });
+  const { data } = useSWR("/odata/Banner", getBanners);
   return (
     <RootComponent>
       <h3>
-        Welcome to{' '}
+        Welcome to{" "}
         {shopReducer.shop.shopDescriptor?.shopOwner ??
           shopReducer?.shop?.domain}
         !🎉
       </h3>
-      <p className='text-welcome'>
+      <p className="text-welcome">
         You will be able to see top performers based on views and
         call-to-actions
       </p>
-      <div style={{ marginTop: '10px' }}>
+      <div style={{ marginTop: "10px" }}>
         <Swiper
-          navigation={true}
-          // pagination={{
-          //   clickable: true,
-          // }}
-          className='swipper'
-          // cssMode={true}
-          // navigation={true}
-          // pagination={true}
-          // mousewheel={true}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          observer={true}
+          observeParents={true}
+          parallax={true}
+          loop
+          pagination
+          loopFillGroupWithBlank
+          centeredSlides={false}
+          navigation={false}
+          watchOverflow={true}
+          autoplay={true}
+          className="swipper"
         >
           {data?.value?.map((item) => (
             <SwiperSlide key={`slider-${item.id}`}>
-              <img src={item.image} alt='BannerImg' />
+              <img src={item.image} alt="BannerImg" className="slide-img" />
             </SwiperSlide>
           ))}
         </Swiper>
