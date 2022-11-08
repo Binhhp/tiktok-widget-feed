2b5b321c16cd05703d72eae9fdab5fc0455571dd
khoapiterrr <khoapiterrr99@gmail.com>
2022-11-09 00:26:18 +0700

    call api post

--- TiktokWidget/ClientApp/src/views/Admin/Dashboard/components/MostPosts/index.tsx
+++ TiktokWidget/ClientApp/src/views/Admin/Dashboard/components/MostPosts/index.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { MostPostRoot } from './style';
 import product from 'assets/images/product.png';
 import MostPostItemComponent from './MostPostItem';
+import { getPosts } from 'repositories/api';
+import useSWR from 'swr';
 
 const MostPost = () => {
+  const { data } = useSWR('/odata/Posts', () => getPosts());
+  const posts = data?.value;
+  if (posts?.length === 0) {
+    return null;
+  }
   return (
     <MostPostRoot>
-      <h4 className="title">Most Popular Posts</h4>
-      <div className="post-list">
-        <MostPostItemComponent
-          img={product}
-          impressionCount={23}
-          clickCount={300}
-        />
-        <MostPostItemComponent
-          img={product}
-          impressionCount={23}
-          clickCount={300}
-        />
-        <MostPostItemComponent
-          img={product}
-          impressionCount={23}
-          clickCount={300}
-        />
+      <h4 className='title'>Most Popular Posts</h4>
+      <div className='post-list'>
+        {posts?.map((item) => (
+          <MostPostItemComponent
+            img={item.image}
+            impressionCount={item.impression}
+            clickCount={item.clicks}
+            key={item.id}
+          />
+        ))}
       </div>
     </MostPostRoot>
   );
